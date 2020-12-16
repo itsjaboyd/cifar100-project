f6361e22e399fa72a1423fd03611bcd085f15962
Jason Boyd <jasonboyd99@gmail.com>
2020-12-16 10:26:49 -0700

    Added random forest imports.

--- raf/cifar_raf.py
+++ raf/cifar_raf.py
@@ -8,3 +8,10 @@
 # This file creates the various random forest network
 # designs using the CIFAR-100 dataset loaded in from TensorFlow.
 #=================================================================
+
+from sklearn import tree, metrics
+import numpy as np
+import random
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+
