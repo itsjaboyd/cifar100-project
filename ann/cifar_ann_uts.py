0e72599d3dce2ab41eebbbb6e73fc46f962b1941
Jason Boyd <jasonboyd99@gmail.com>
2020-12-15 17:27:26 -0700

    Beginning file structure for ANN.

---
+++ ann/cifar_ann_uts.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+#=================================================================
+# Author: Jason Boyd
+# USU A#: A02258798
+#
+# This file contains unit tests for the cifar ann files.
+#=================================================================
+
+import unittest
+import tensorflow as tf
+# import useful functionsn from cnn driver
+import cnn.cifar_cnn_driver as cdr
